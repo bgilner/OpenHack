2efd7296b51cd114cf3caa85c5f5bb7c21779c5d
Brian Gilner <brian.gilner@olo.com>
2018-09-27 15:42:53 -0400

    fix

--- OpenHack/GetRatings.cs
+++ OpenHack/GetRatings.cs
@@ -1,9 +1,12 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Security;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Documents;
 using Microsoft.Azure.Documents.Client;
 using Microsoft.Azure.Documents.Linq;
 using Microsoft.Azure.WebJobs;
@@ -18,27 +21,30 @@ namespace OpenHack
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, nameof(HttpMethod.Get), Route = null)]
             HttpRequest req,
-            ILogger log,
-            [CosmosDB(
-                databaseName: "ProductRatings",
-                collectionName: "Items",
-                ConnectionStringSetting = "CosmosDBConnection" //,
-//                SqlQuery = "select * from Items c where c.userId=\"{Query.userId}\""
-            )]
-            DocumentClient client)
-
+            ILogger log)
         {
+
             log.Log(LogLevel.Information, "C# HTTP trigger function processed a get ratings request.");
+            var authKey = new SecureString();
+
+            foreach (char c in "qNdQLb6Edv2yN90KJYejQNXsr4YOEFMdbvbxz3h3ma3UfS9fHrzKXbvBjTQWxtMJKnIwBqQ7V5qHdxOomCQxoQ==")
+            {
+                authKey.AppendChar(c);
+            }
+
+            var client = new DocumentClient(new Uri("https://openhack-team-8.documents.azure.com:443/"), authKey);
             var collectionUri = UriFactory.CreateDocumentCollectionUri("ProductRatings", "Items");
             var userId = req.Query["userId"];
-            var query = client.CreateDocumentQuery<RatingModel>(collectionUri)
-                .Where(x => x.UserId == userId)
-                .AsDocumentQuery();
+            var sqlQuery = new SqlQuerySpec("SELECT * FROM Items c WHERE c.userId=@userId",
+                new SqlParameterCollection(new[] {new SqlParameter("@userId", Guid.Parse(userId))}));
 
-            var ratings = new List<RatingModel>();
+            var query = client.CreateDocumentQuery<RatingModel>(collectionUri, sqlQuery).AsDocumentQuery();
+               
+            var ratings = new List<FeedbackViewModel>();
+
             while (query.HasMoreResults)
             {
-                foreach (var rating in await query.ExecuteNextAsync())
+                foreach (var rating in await query.ExecuteNextAsync<RatingModel>())
                 {
                     ratings.Add(rating.ToViewModel());
                 }
